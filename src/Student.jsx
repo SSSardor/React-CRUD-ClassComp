07017ea48cfc56ec624e62174be37a5a2d4e5593
SSSardor <sardorsamijonov1226@gmail.com>
2022-05-19 07:42:31 +0500

    continue

--- src/Student.jsx
+++ src/Student.jsx
@@ -7,77 +7,177 @@ class Student extends React.Component {
     super(props);
     this.state = {
       student,
-      selected:null
+      name: "",
+      status: "",
+      age: "",
+      selected: {},
     };
   }
-
+  // *************************************************
   render() {
-
+    //delete
     const onDelete = (id) => {
       const res = this.state.student.filter((value) => value?.id !== id);
       this.setState({ student: res });
     };
-
-      const onFilter = ({ target }) => {
+    // search
+    const onFilter = ({ target }) => {
       const { value, name } = target;
-      const res = student.filter((item) =>`${item[name]}`.toLowerCase().includes(value.toLowerCase()));
+      const res = student.filter((item) =>
+        `${item[name]}`.toLowerCase().includes(value.toLowerCase())
+      );
       this.setState({ student: res });
     };
-    // const onEdit=(id)=>{
-    //   cons res=this.state.selected
-    // }
-    return (
+    //edit
 
-      <div class="box1">
+    const onSelect = (value) => {
+      this.setState({ selected: value });
+    };
+    // input disable
+    const getDisabled = (id) => {
+      return this.state.selected?.id !== id;
+    };
+    //Cancel
+    const onCancel = () => {
+      this.setState({ selected: null });
+    };
+    //Save
+    const onSave = () => {
+      console.log(this.state);
+      let res = this.state.student.map((value) => {
+        return this.state.selected?.id === value.id
+          ? {
+              ...value,
+              name: this.state.name,
+              age: this.state.age,
+              status: this.state.status,
+            }
+          : value;
+      });
+      console.log(res, "resss");
+      this.setState({ student: res, selected: null });
+    };
+    //Change
+    const onChange = ({ target }) => {
+      const { name, value } = target;
+      this.setState({ [name]: value });
+    };
+
+    //********************************* */
+    return (
+      <div className="box1">
         <div>
           <h1>Students table </h1>
         </div>
         <div className="input">
-        <input onChange={onFilter} name="id" type="number" placeholder="ID" />
-        <input onChange={onFilter} name="name" type="text" placeholder="Name" />
-        <input onChange={onFilter} name="status" type="text" placeholder="Status"/>
-        <input onChange={onFilter} name="age" type="number" placeholder="Age" />
+          <input onChange={onFilter} name="id" type="number" placeholder="ID" />
+          <input
+            onChange={onFilter}
+            name="name"
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            onChange={onFilter}
+            name="status"
+            type="text"
+            placeholder="Status"
+          />
+          <input
+            onChange={onFilter}
+            name="age"
+            type="number"
+            placeholder="Age"
+          />
         </div>
-         <div className="box">
-         <table border="2px">
-          <thead>
-            <tr>
-              <th className="id">ID</th>
-              <th>Name</th>
-              <th>Status</th>
-              <th>Age</th>
-              <th>Delete/Edit</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.student.map((value) => {
-              return (
-                <tr key={value.id}>
-                  <td className="id">{value.id}</td>
-                  <td>{value.name} </td>
-                  <td>{value.status}</td>
-                  <td>{value.age}</td>
-                  <td>
-                    <div className="btn">
-                    <div class="sardor">
-                    <button className="btn1" onClick={() => onDelete(value.id)}>delete</button>
-                    </div>
-                      {/* <div>
-                     {!this.state.selected ?(
-                      <button className="btn1" onClick={this.setState({selected:value.id})}>update</button>
-                     ):(
-                      <button className="btn1" onClick={this.setState({selected:null})}>save</button>
-                     )} 
-                    </div>   */}
-                    </div> 
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-         </div>
-        
+        {/* *************************************************** */}
+        <div className="box">
+          <table border="2px">
+            <thead>
+              <tr>
+                <th className="id">ID</th>
+                <th>Name</th>
+                <th>Status</th>
+                <th>Age</th>
+                <th>Delete/Edit</th>
+              </tr>
+            </thead>
+            {/***************************/}
+            <tbody>
+              {this.state.student.map((value) => {
+                const { id, name, status, age } = value;
+                return (
+                  <tr key={value.id}>
+                    <td className="id">
+                      <input disabled={true} defaultValue={id} type="text" />
+                    </td>
+                    <td>
+                      <input
+                        disabled={getDisabled(id)}
+                        defaultValue={name}
+                        type="text"
+                        onChange={onChange}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        disabled={getDisabled(id)}
+                        defaultValue={status}
+                        type="text"
+                        onChange={onChange}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        disabled={getDisabled(id)}
+                        defaultValue={age}
+                        type="text"
+                        onChange={onChange}
+                      />
+                    </td>
+                    <td>
+                      <div className="sardor">
+                        {!getDisabled(id) ? (
+                          <>
+                            <button
+                              className="btn1"
+                              onClick={() => onCancel(id)}
+                            >
+                              {" "}
+                              cancel
+                            </button>
+                            <button
+                              className="btn1"
+                              onClick={() => onSave(value)}
+                            >
+                              save
+                            </button>
+                          </>
+                        ) : (
+                          <>
+                            <button
+                              className="btn1"
+                              onClick={() => onDelete(id)}
+                            >
+                              delete{" "}
+                            </button>
+                            <button
+                              className="btn1"
+                              onClick={() => onSelect(value)}
+                            >
+                              {" "}
+                              edit{" "}
+                            </button>
+                          </>
+                        )}
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
